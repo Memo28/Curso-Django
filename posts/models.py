23d74e23c4b2dffb133a3769a241696ca942fe92
Dev-01 <development@evonova.mx>
2019-09-10 17:44:48 -0500

    Added DB

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
+#For the ORM
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
