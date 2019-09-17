b7e3bebe74592827b8e5e6aaa7a976c0f48a2517
Memo <memovdg@gmail.com>
2019-09-16 19:30:45 -0500

    Changes in the model

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 
-class Users(models.Model):
+class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
@@ -13,9 +13,15 @@ class Users(models.Model):
 
     is_admin = models.BooleanField(default=False)
 
-    bio = models.TextField(blank=True)
-
+    bio = models.TextField(blank=True, default='Something cool here')
     birthdate = models.DateField(blank=True, null=True)
+
+    country = models.TextField(blank=True)
+    city = models.TextField(blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+
+def __str_(self):
+    return self.email
