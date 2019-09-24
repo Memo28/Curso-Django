f6cef70d537d90e1c0c79e1c2437ac8be48ae9be
Memo <memovdg@gmail.com>
2019-09-23 22:43:10 -0500

    Add users

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -22,7 +22,7 @@ from posts import views as post_views
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('hello/',local_views.hello),
     path('sorted/', local_views.sorted),
     path('hi/<str:name>/<int:age>/', local_views.hi),
