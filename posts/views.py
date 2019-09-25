a4659308dddd314de1f0355d11a7e64dfc6851c6
Dev-01 <development@evonova.mx>
2019-09-25 12:43:13 -0500

    Display the posts

--- posts/views.py
+++ posts/views.py
@@ -1,43 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+"""Posts views."""
 
+# Django
+from django.shortcuts import render
+
+# Utilities
 from datetime import datetime
-# Create your views here.
+
 
 posts = [
     {
-        'title' : 'Forest',
-        'user' : {
-            'name' : 'Pepito',
-            'picture' : 'https://picsum.photos/id/933/200/200'
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
         },
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo' : 'https://picsum.photos/id/933/200/200'
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'title' : 'River',
-        'user' : {
-            'name' : 'Pepito',
-            'picture' : 'https://picsum.photos/id/56/200/200'
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
         },
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo' : 'https://picsum.photos/id/55/200/200'
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'title' : 'City',
-        'user' : {
-            'name' : 'Pepito',
-            'picture' : 'https://picsum.photos/id/12/200/200'
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
         },
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo' : 'https://picsum.photos/id/56/200/200'
-    },
-
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 
 def list_views(request):
-    return render(request,'feed.html', {'posts' : posts})
-
+    """List existing posts."""
+    return render(request, 'posts/feed.html', {'posts': posts})
 
 
