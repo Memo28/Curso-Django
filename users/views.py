56bcbefbc62a00065b395043ae5d54158572612c
Dev-01 <development@evonova.mx>
2019-09-26 17:41:16 -0500

    Signup

--- users/views.py
+++ users/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User
+from users.models import Profile
+
 
 def login_view(request):
 
@@ -15,7 +21,36 @@ def login_view(request):
         if user:
             login(request, user)
             return redirect('feed')
-        else:
+        try:
             return render(request, 'users/login.html',{'error': 'Invalid username and password'})
+        except:
+            
 
     return render(request,'users/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+
+    return redirect('login')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if  password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'} )
+        
+        user = User.objects.create_user(username=username, password=password)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
