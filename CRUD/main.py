07c95c4f9828a72279baef776f9de48ca6ef2fe2
MemoVDG <memovdg@gmail.com>
2020-03-02 15:53:55 -0600

    Seccion 4

--- CRUD/main.py
+++ CRUD/main.py
@@ -1,49 +1,85 @@
-clients = 'pablo,ricardo,'
+import sys
+clients = [
+    {
+        'name': 'Pablo',
+        'company': 'Google',
+        'email': 'pablo@gmail.com',
+        'position': 'Software engineer'
+    },
+    {
+        'name': 'Toño',
+        'company': 'Amazon',
+        'email': 'toño@facebook.com',
+        'position': 'Data enginieer'
+    }
+]
 
-def create_client(client_name):
+
+def create_client(client):
     global clients
-    if client_name not in clients:
-        clients += client_name
-        add_comma()
+    if client not in clients:
+        clients.append(client_name)
     else:
         print("Client already is in the client's list")
 
+
 def list_clients():
     global clients
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid=idx, name=client['name'], company=client['company'], email=client['email'], position=client['position']))
 
-def add_comma():
-    global clients
-    clients += ','
 
-def update_client(client_name, update_client_name):
+def update_client(client_name):
     global clients
-    
-    if client_name in clients:
-        clients = clients.replace(client_name + ',', update_client_name)
-        add_comma()
+
+    user_id = _exist_user(client_name)
+    if user_id:
+        clients[user_id] = {
+            'name': _get_client_field('name'),
+            'company': _get_client_field('company'),
+            'email': _get_client_field('email'),
+            'position': _get_client_field('position')
+        }
         list_clients()
     else:
         print('Client is not in clients list')
 
+
 def delete_client(client_name):
     global clients
 
-    if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+    user_id = _exist_user(client_name)
+    if user_id:
+        clients.pop(user_id)
     else:
         print('Client is not in clients list')
 
+
+def _exist_user(client_name):
+    for idx, client in enumerate(clients):
+        if client['name'] == client_name:
+            return idx
+
+    return False
+
+
 def search_client(client_name):
     global clients
-
-    client_list = clients.split(',')
-    for client in client_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
+
+def _get_client_field(field_name):
+    field = None
+
+    while not field:
+        field = input('Whats the client {} ? : '.format(field_name))
+
+    return field
 
 
 def prints_welcome():
@@ -55,8 +91,20 @@ def prints_welcome():
     print('[U] Update client')
     print('[S] Search client')
 
+
 def _get_client_name():
-    return input('What is the client name? : ') 
+    client_name = None
+    while not client_name:
+        client_name = input('What is the client name? : ')
+
+        if client_name == 'exit':
+            client_name == None
+            break
+
+    if not client_name:
+        sys.exit()
+    return client_name
+
 
 if __name__ == '__main__':
     prints_welcome()
@@ -64,8 +112,13 @@ if __name__ == '__main__':
     command = input().upper()
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
+        client = {
+            'name': _get_client_field('name'),
+            'company': _get_client_field('company'),
+            'email': _get_client_field('email'),
+            'position': _get_client_field('position')
+        }
+        clients.append(client)
         list_clients()
     elif command == 'D':
         client_name = _get_client_name()
@@ -73,11 +126,10 @@ if __name__ == '__main__':
         list_clients()
     elif command == 'U':
         client_name = _get_client_name()
-        update_client_name = input('What is the new name? : ')
-        update_client(client_name, update_client_name)
-    elif  command == 'S':
+        update_client(client_name)
+    elif command == 'S':
         client_name = _get_client_name()
-        found = search_client(client_name)
+        found = _exist_user(client_name)
         if found:
             print('The client is in the client list')
         else:
